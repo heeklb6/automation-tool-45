a8a385452a91e65f729113fedbc0d90de2e0dd4b
Martin Novák <j66634062@gmail.com>
2026-08-14 17:14:25 +0200

    chore:cleanup and code reorganization

--- core.py
+++ core.py
@@ -1,30 +1,30 @@
-import time
 import requests
 
-class CryptoDataFetcher:
-    def __init__(self, api_url):
-        self.api_url = api_url
-        self.cache = {}  # To store API results
-        self.cache_time = 300  # Cache expiration time in seconds
-        self.last_fetched_time = 0
+class CryptoAPI:
+    BASE_URL = 'https://api.coingecko.com/api/v3/'
 
-    def get_data(self, crypto_symbol):
-        current_time = time.time()
-        # Return cached data if it's still valid
-        if crypto_symbol in self.cache and (current_time - self.last_fetched_time) < self.cache_time:
-            return self.cache[crypto_symbol]
-        # Fetch new data from the API if cache is expired or not available
-        response = requests.get(f'{self.api_url}/{crypto_symbol}')
+    def get_price(self, coin_id):
+        url = f'{self.BASE_URL}simple/price?ids={coin_id}&vs_currencies=usd'
+        response = requests.get(url)
         if response.status_code == 200:
-            self.cache[crypto_symbol] = response.json()
-            self.last_fetched_time = current_time
-            return self.cache[crypto_symbol]
+            return response.json()
         else:
-            response.raise_for_status()
+            raise Exception('API request failed')
 
-# Example usage
+    def get_market_data(self, coin_id):
+        url = f'{self.BASE_URL}coins/{coin_id}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception('API request failed')
+
 if __name__ == '__main__':
-    fetcher = CryptoDataFetcher('https://api.coingecko.com/api/v3/simple/price')
-    print(fetcher.get_data('bitcoin'))
-    time.sleep(10)  # Simulate delay
-    print(fetcher.get_data('bitcoin'))  # Should return cached data
+    crypto_api = CryptoAPI()
+    try:
+        price = crypto_api.get_price('bitcoin')
+        market_data = crypto_api.get_market_data('bitcoin')
+        print('Bitcoin Price:', price)
+        print('Market Data:', market_data)
+    except Exception as e:
+        print('Error:', e)
